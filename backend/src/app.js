a7e8c7f9c528b77bee315ee4ce2433f7332ab491
devsagarkumarjha <devsagarkumarjha@gmail.com>
2025-04-24 08:36:02 +0530

    Initialized project created boilerplate code

---
+++ backend/src/app.js
@@ -0,0 +1,8 @@
+import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+
+const app = express();
+
+app.use("api/v1/auth", authRoutes);
+
+export default app;
---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,7 @@
+import express from "express"
+
+
+const authRoutes = express(); 
+
+
+export default authRoutes;
