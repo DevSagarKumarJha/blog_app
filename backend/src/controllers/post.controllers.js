bbce5cb88803bfa879a74cc15e5ad4809e3f02e0
devsagarkumarjha <devsagarkumarjha@gmail.com>
2025-04-24 13:40:48 +0530

    added: getPostbyId method

--- backend/src/controllers/post.controllers.js
+++ backend/src/controllers/post.controllers.js
@@ -40,4 +40,23 @@ const getAllPosts = async (req, res) => {
   }
 };
 
-export { createPost, getAllPosts};
+const getPostById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await db.post.findUnique({
+      where: { id },
+      include: {
+        author: { select: { id: true, username: true, email: true } },
+      },
+    });
+
+    if (!post) return res.status(404).json({ error: "Post not found" });
+
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ error: "Error fetching post" });
+  }
+};
+
+export { createPost, getAllPosts, getPostById };
--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import authenticate from "../middlewares/auth.middleware.js";
 import upload from "../middlewares/upload.middleware.js";
-import { createPost, getAllPosts } from "../controllers/post.controllers.js";
+import { createPost, getAllPosts, getPostById } from "../controllers/post.controllers.js";
 
 const postRoutes = express.Router();
 
@@ -10,4 +10,5 @@ postRoutes
   .post(authenticate, upload.single("image"), createPost)
   .get(getAllPosts);
 
+postRoutes.route("/:id").get(getPostById);
 export default postRoutes;
